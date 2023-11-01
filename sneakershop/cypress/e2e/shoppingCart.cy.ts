1d4420bfa834b1a8c0d1951457f1492bc695baaf
ainhoaconde <90756914+ainhoaconde@users.noreply.github.com>
2023-11-01 20:56:41 +0100

    added comments

--- sneakershop/cypress/e2e/shoppingCart.cy.ts
+++ sneakershop/cypress/e2e/shoppingCart.cy.ts
@@ -1,21 +1,22 @@
 describe("Add to Shopping Cart", () => {
-    it("should add an item to the shopping cart", () => {
-      // Visit the page where the ItemCard component is rendered
+  it("should add an item to the shopping cart", () => {
+      // Visit the homepage where the ItemCard component is rendered
       cy.visit("/");
-  
+
       // Intercept the route to the product details page (adjust the route pattern as needed)
       cy.intercept("/shop/*").as("productDetails");
-  
+
+      // Click on the "View options" button of the first item
       cy.get(".cardButton:contains('View options')").first().click();
       cy.wait(1000); // Adjust the wait time as needed
-      
-      // Assert that you are now on the product details page
-      cy.url().should("match", /\/shop\/\w+/); // Adjust the URL pattern as needed
-  
-      // Click the "Add to Cart" button
-      cy.get("button:contains('Add to cart')").click();      // Confirm that the item was added to the shopping cart
-      cy.get(".shoppingCart button").click();
 
-    });
+      // Assert that you are now on the product details page (verify the URL)
+      cy.url().should("match", /\/shop\/\w+/); // Adjust the URL pattern as needed
+
+      // Click the "Add to Cart" button
+      cy.get("button:contains('Add to cart')").click();
+
+      // Confirm that the item was added to the shopping cart by clicking on the shopping cart icon
+      cy.get(".shoppingCart button").click();
   });
-  
+});
